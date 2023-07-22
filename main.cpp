5acd00a0c3a6df2c7f4168dc3c4905af9c5f40a4
DPDL91 <lyonzy2@gmail.com>
2023-07-22 11:48:09 +0000

    DLyons CA1 Q4
    Signed-off-by: DPDL91 <lyonzy2@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "mbed.h"
+
+// main() runs in its own thread in the OS
+int main()
+
+//Initalize variables
+
+{
+    int a = 10;
+    int b = 2;
+    int c = 30;
+    int d, e;
+
+    //add values of a and b and store in d
+
+    d = a + b;
+    printf("Value of d: %d/n", d);
+
+
+    //subtract value of c from d and store it in e
+
+    e = d - c;
+    printf("Value of e: %d/n", e);
+
+    //End program
+    return 0;
+    {
+
+    }
+}
+
